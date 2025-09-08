0fe657386f51110ce271cdeab64102fe6632ffe8
bonsonalo <bonson2468@gmail.com>
2025-09-09 07:22:36 +0800

    feat: added automation code that I can use over and over for missing values, duplicates

--- week03/week03_project/eda_dashboard/src/file_utils.py
+++ week03/week03_project/eda_dashboard/src/file_utils.py
@@ -22,8 +22,53 @@ logging.basicConfig(
 
 def load_data(file_path):
     if os.path.isfile(file_path):
-        df = pd.read_csv(file_path)
-        return df
+        if file_path.endswith(".csv"):
+            df = pd.read_csv(file_path)
+            return df
+        elif file_path.endswith(".json"):
+            df = pd.read_json(file_path)
+            return df
+        elif file_path.endswith("xlsx") or file_path.endswith("xls"):
+            df = pd.read_excel(file_path)
+            return df
+        else:
+            logging.error("Only .csv, .xls, .xlsx, .json files are possible")
     else:
+        raise FileNotFoundError(f"the {file_path} is not found")
         logging.error(f"The file {file_path} doesn't exist")
-        return None
+
+
+
+
+""" Missing value report """
+
+
+def missing_values_report(df):
+
+    missing = df.isna().sum()
+    pct_missing = round(missing / len(df) * 100, 2)
+
+
+    report = pd.DataFrame(
+        {
+            "n_missing": missing,
+            "pct_missing": pct_missing
+        }
+    )
+
+    report = report[report["n_missing"] > 0]      ## keep only columns with missing
+    return report.sort_values(by="pct_missing", ascending=False)
+
+
+
+
+def check_duplicates(df):
+
+    duplicate_mask = df.duplicated(keep= False)   # this returns a bool. and keep = False means that list all duplicates as duplicates
+
+    duplicates = df[duplicate_mask]
+    n_duplicates = len(duplicates)
+
+    logging.info(f"Found {n_duplicates} duplicate rows")
+
+    return duplicates, n_duplicates
