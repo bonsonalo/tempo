07c9f0d3c0094dee73d19b08ec7cdc019b151580
bonsonalo <bonson2468@gmail.com>
2025-10-04 11:07:08 +0800

    feat: add log in config file

--- week05/document-processor/src/document_processor/config.py
+++ week05/document-processor/src/document_processor/config.py
@@ -5,3 +5,6 @@ from pathlib import Path
 def setup_logging(log_level= logging.INFO):
     logs_dir = Path("logs")
     logs_dir.mkdir(exist_ok=True)
+
+
+    log_format= "%(asctime)s - %(levelname)s - %(message)s"
