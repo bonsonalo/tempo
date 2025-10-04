53bfd2645cee69c6d9b6288d96a00eda83b29026
bonsonalo <bonson2468@gmail.com>
2025-10-04 11:22:42 +0800

    feat: add user_service.py file

---
+++ week05/task_management_system/Services/user_service.py
@@ -0,0 +1,15 @@
+from Models.User import User
+
+
+class User_Service():
+    def __init__(self):
+        self.users= {}
+
+    def add_user(self, name, id, email):
+        user= User(name, id, email)
+        self.users[id]= user
+        return user
+
+
+    def get_user(self, id):
+        return self.users.get(id)
