cd51db3f32735b1cd3b8a249cc288386f1f13b2f
bonsonalo <bonson2468@gmail.com>
2025-10-05 12:08:31 +0800

    refactor: add async functionality

--- week05/document-processor/src/document_processor/pipeline.py
+++ week05/document-processor/src/document_processor/pipeline.py
@@ -3,21 +3,27 @@ from .models import ProcessingResult
 from .utils import detect_file_type
 from .processors import process_csv_file, process_json_file, process_pdf_file
 from typing import List
+import asyncio
+
+import logging
 
 
 
 
-def process_single_file(file_path: Path) -> ProcessingResult:
 
+async def process_single_file(file_path: Path) -> ProcessingResult:
+
+    logging.info(f"processing: {file_path}")
     detected_type= detect_file_type(file_path)
 
     if detected_type== "csv":
-        return process_csv_file(file_path)
+        return await process_csv_file(file_path)
     elif detected_type == "json":
-        return process_json_file(file_path)
+        return await process_json_file(file_path)
     elif detected_type == "pdf":
-        return process_pdf_file(file_path)
+        return await process_pdf_file(file_path)
     else:
+        logging.warning(f"Skipping Unsupported file type: {file_path.suffix} - {file_path}")
         return ProcessingResult(
             file_path=file_path,
             file_type="Unknown",
@@ -28,11 +34,23 @@ def process_single_file(file_path: Path) -> ProcessingResult:
 
 
 
-def process_multiple_files(file_paths: List[Path]) -> List[ProcessingResult]:
+async def process_multiple_files(file_paths: List[Path], max_concurrent: int=10) -> List[ProcessingResult]:
 
-    processed_list= []
-    for file_path in file_paths:
-        processed= process_single_file(file_path)
-        processed_list.append(processed)
+    semaphore = asyncio.Semaphore(max_concurrent)
+    
+    async def process_with_semaphore(file_path: Path):
+        async with semaphore:
+            return await process_single_file(file_path)
 
-    return processed_list
+
+    tasks = [process_with_semaphore(file_path) for file_path in file_paths]
+
+    results= await asyncio.gather(*tasks)
+    for result in results:
+        if result.success:
+            logging.info(f"✓ Success: {result.file_path.name} - {result.rows_processed} rows")
+        else:
+            logging.error(f"✗ Failed: {result.file_path.name} - {result.error_message}")
+        
+
+    return list(results)
