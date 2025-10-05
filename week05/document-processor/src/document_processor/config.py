f6ba07432656359c29ab6481a07ef41ca34a7ea2
bonsonalo <bonson2468@gmail.com>
2025-10-05 12:06:43 +0800

    feat: add logging setup

--- week05/document-processor/src/document_processor/config.py
+++ week05/document-processor/src/document_processor/config.py
@@ -2,9 +2,16 @@ import logging
 import sys
 from pathlib import Path
 
-def setup_logging(log_level= logging.INFO):
-    logs_dir = Path("logs")
-    logs_dir.mkdir(exist_ok=True)
+def setup_logging():
+     
+    Path("logs").mkdir(exist_ok=True)
 
-
-    log_format= "%(asctime)s - %(levelname)s - %(message)s"
+    logging.basicConfig(
+        level=logging.INFO,
+        format= "%(asctime)s - %(levelname)s - %(message)s",
+        handlers=[
+            logging.FileHandler('logs/processing.py'),
+            logging.StreamHandler()
+        ]
+    )
+    
