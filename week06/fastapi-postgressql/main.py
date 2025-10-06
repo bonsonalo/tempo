6240ad16d9b7aef2445c9871a5b3875ae1abfea0
bonsonalo <bonson2468@gmail.com>
2025-10-07 01:01:20 +0800

    feat: add /questions/ endpoint

--- week06/fastapi-postgressql/main.py
+++ week06/fastapi-postgressql/main.py
@@ -28,3 +28,16 @@ def get_db():
         db.close()
 
 db_dependency= Annotated[Session, Depends(get_db)]
+
+
+@app.post("/questions/")
+async def create_questions(question: QuestionBase, db= db_dependency):
+    db_question= models.Questions(question_text= question.question_text)
+    db.add(db_question)
+    db.commit()
+    db.refresh(db_question)
+    for choice in question.choices:
+        db_choice= models.Choices(choice_text= choice.choice_text, is_correct= choice.is_correct)
+        db.add(db_choice)
+    db.commit()
+
