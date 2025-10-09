c3ebe0e64f4177388a16d6e667ec5d85a566328d
bonsonalo <bonson2468@gmail.com>
2025-10-10 04:23:20 +0800

    chore: adjust the codes to make it modular and fix the import problem

--- week06/jwt_practice/main.py
+++ week06/jwt_practice/main.py
@@ -1,15 +1,21 @@
 from fastapi import FastAPI, HTTPException, status, Depends
-import models
-from database import SessionLocal, engine
-from typing import Annotated
-from sqlalchemy.orm import Session
-from pydantic import BaseModel
-from config import get_db
+from jwt_practice import models
+from .database import engine
+from .config import db_dependency, user_dependency
+from . import auth
+
 
 app= FastAPI()
+app.include_router(auth.router)
 
-models.Base.metadata.create_all(bind= engine)
+models.base.metadata.create_all(bind= engine)
 
-db_dependency= Annotated[Session, Depends(get_db)]
+
+
+@app.get("/", status_code=status.HTTP_200_OK)
+async def user(user: user_dependency, db: db_dependency):
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authentication failed")
+    return {"user": user}
 
 
