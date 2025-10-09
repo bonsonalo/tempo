4ede89afa6bc561259773eb093f17d99ac927e0a
bonsonalo <bonson2468@gmail.com>
2025-10-10 04:21:48 +0800

    chore: adjust the codes to make it modular and finalize the code

--- week06/jwt_practice/config.py
+++ week06/jwt_practice/config.py
@@ -1,4 +1,11 @@
-from database import SessionLocal
+from .database import SessionLocal
+from typing import Annotated
+from sqlalchemy.orm import Session
+from fastapi import Depends, HTTPException
+from jose import JWTError, jwt 
+from fastapi.security import OAuth2PasswordBearer
+from starlette import status
+
 
 
 def get_db():
@@ -7,3 +14,28 @@ def get_db():
         yield db
     finally:
         db.close()
+
+db_dependency= Annotated[Session, Depends(get_db)]
+
+
+
+SECRET_KEY= "222370886cab0b0fc09a75839c9087e31c435307586e27755888952261d0ee95"
+ALGORITHM= "HS256" 
+
+
+oauth2_bearer= OAuth2PasswordBearer(tokenUrl="auth/token")
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload= jwt.decode(token, SECRET_KEY, algorithms= [ALGORITHM])
+        username: str = payload.get("sub")
+        user_id: int = payload.get("id")
+
+        if username is None or user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="could not validate user")
+        return {"username": username, "id": user_id}
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="could not validate user")
+    
+
+user_dependency= Annotated[dict, Depends(get_current_user)]
