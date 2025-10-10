6e4f7d24d24e76c51572ab706df44f4bd8b043ed
bonsonalo <bonson2468@gmail.com>
2025-10-11 03:58:06 +0800

    feat: add sign up, log in, and access protected data functionality

---
+++ week06/backend/app/api/v1/endpoints/auth.py
@@ -0,0 +1,75 @@
+from datetime import datetime, timedelta, timezone
+from fastapi import APIRouter, Depends, HTTPException
+from starlette import status
+from pydantic import BaseModel
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from typing import Annotated
+from starlette import status
+from jose import jwt
+
+
+from backend.app.core.config import ALGORITHM, SECRET_KEY, db_dependency
+from passlib.context import CryptContext
+from backend.app.model.models import User
+
+
+
+
+router= APIRouter(
+    prefix= "/auth",
+    tags= ["auth"]
+)
+
+class CreateUserRequest(BaseModel):
+    user_name: str
+    password: str
+
+class Token(BaseModel):
+    token_type: str
+    access_token: str
+
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated= "auto")
+
+# for sign up
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_user(user: CreateUserRequest, db: db_dependency):
+    user_request_model= User(
+        username= user.user_name,
+        hashed_password= bcrypt_context.hash(user.password)
+    )
+    db.add(user_request_model)
+    db.commit()
+
+
+
+# for login
+@router.post("/token", response_model= Token)
+async def login_for_access_token(user_form: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
+    user = authenticate_user(user_form.username, user_form.password, db)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail= "couldnot validate the user")
+    token= create_access_token(user.username, user.id, timedelta(minutes=20))
+
+    return {"access_token": token, "token_type": "bearer"}
+
+
+
+def authenticate_user(username: str, password: str, db: db_dependency):
+    user = db.query(User).filter(User.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return user
+
+def create_access_token(username: str, user_id: int, expires_delta: timedelta):
+    encode = {"sub": username, "id": user_id}
+    expires= datetime.now(timezone.utc) + expires_delta
+    encode.update({"exp": expires})
+
+    return jwt.encode(encode, SECRET_KEY, algorithm= ALGORITHM)
+
+
+
+
