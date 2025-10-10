139e73d27c48d9445c241039444767b5da204853
bonsonalo <bonson2468@gmail.com>
2025-10-11 04:00:37 +0800

    add code to include auth router, and add user_dependency in the get endpoint and other codes

---
+++ week06/backend/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, HTTPException
+from backend.app.core import database
+from backend.app.core.database import engine, base
+from starlette import status
+from backend.app.core.config import db_dependency, user_dependency
+from backend.app.api.v1.endpoints import auth
+
+
+app= FastAPI()
+app.include_router(auth.router)
+
+database.base.metadata.create_all(bind= engine)
+
+
+@app.get("/user", status_code=status.HTTP_200_OK)
+def user(user: user_dependency, db: db_dependency):
+    if user in None:
+        raise HTTPException(status_code=401, detail="user not validated")
+    return {"User": user}
+
