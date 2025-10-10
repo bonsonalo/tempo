b8ff5a622ef24a44b311ada172688d76d037b53d
bonsonalo <bonson2468@gmail.com>
2025-10-11 03:59:09 +0800

    feat: add code for oauth_bearer, get_current_user code and other codes

---
+++ week06/backend/app/core/config.py
@@ -0,0 +1,39 @@
+from .database import SessionLocal
+from typing import Annotated
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, Depends
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
+from starlette import status
+
+
+
+def get_db():
+    db= SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency= Annotated[Session, Depends(get_db)]
+
+SECRET_KEY= "6dbd5e4d96033cb463bfd0d92bb67895079307d3b691bfba03ca50c3c01fdd2e"
+ALGORITHM= "HS256"
+oauth_bearer= OAuth2PasswordBearer(tokenUrl="auth/token") # if there was front end, there would have been a ssetup in the front end we dont have to call "auth/token", but since only abckend , when user try to access or request some file, it will tell them to give credentials and then checks that one
+
+def get_current_user(token: Annotated[str, Depends(oauth_bearer)]):
+    try:
+        payload= jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        username:str = payload.get("sub")
+        user_id: int= payload.get("id")
+
+        if username is None or user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="couldnot be validated")
+        return {"username": username, "id": user_id}
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="couldnot be validated")
+    
+
+user_dependency= Annotated[dict, Depends(get_current_user)]
+
+
