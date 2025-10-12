0922d82eb6cdd1f05991834e1b3633e0a950e4eb
bonsonalo <bonson2468@gmail.com>
2025-10-12 10:21:41 +0800

    feat: loaded .env

--- week06/backend/app/core/database.py
+++ week06/backend/app/core/database.py
@@ -2,9 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
+from dotenv import load_dotenv
 
 
-DATABASE_URL=os.envget("DB_URL")
+load_dotenv()
+DATABASE_URL=os.getenv("DB_URL")
 
 engine= create_engine(DATABASE_URL)
 
