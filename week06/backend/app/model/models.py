cc82ed8383b884e5c822ecf63ee41b3f24338d98
bonsonalo <bonson2468@gmail.com>
2025-10-14 04:48:08 +0800

    feat: add tables for the inventory

--- week06/backend/app/model/models.py
+++ week06/backend/app/model/models.py
@@ -19,9 +19,10 @@ class Products(base):
     price= Column(DECIMAL, index=True)
     SKU= Column(String, unique=True)
     category_id= Column(Integer, ForeignKey("categories.id", ondelete= "CASCADE"), nullable=False) #nullable=Flase means a post must belong to a user; it cannot exist without one.
-    category= relationship("Categories")
+    category= relationship("Categories", back_populates="product")
     supplier_id= Column(Integer, ForeignKey("suppliers.id", ondelete="CASCADE"), nullable=False) # use ondelete="CASCADE" to automatically delete related records when the parent is deleted
-    supplier= relationship("Suppliers")
+    supplier= relationship("Suppliers", back_populates="supplier_product")
+    stock= relationship("Stock", back_populates="product_stock")
 
 
 class Categories(base):
@@ -30,6 +31,7 @@ class Categories(base):
 
     id= Column(Integer, primary_key=True, index=True, autoincrement=True)
     name= Column(String)
+    product= relationship(Products, back_populates="category")
 
 
 class Suppliers(base):
@@ -37,13 +39,17 @@ class Suppliers(base):
 
     id= Column(Integer, primary_key=True, index=True, autoincrement=True)
     name= Column(String)
+    supplier_product= relationship("Products", back_populates="supplier")
+    stock= relationship("Stock", back_populates="supplier_stock")
 
 class Stock(base):
     __tablename__= "stock"
 
     id= Column(Integer, primary_key=True, index=True, autoincrement=True)
     product_id= Column(Integer, ForeignKey("products.id", ondelete="CASCADE"), nullable=False)
+    product_stock= relationship("Products", back_populates="stock")
     quantity= Column(Integer)
     movement_type= Column(String)
-    date=Column(Date)
+    date= Column(Date)
     supplier_id= Column(Integer, ForeignKey("suppliers.id", ondelete="CASCADE"))
+    supplier_stock = relationship("Suppliers", back_populates="stock")
