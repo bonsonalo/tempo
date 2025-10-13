6de3fb4cc9a3b09411646e77be3d918fcb1b5ff2
bonsonalo <bonson2468@gmail.com>
2025-10-14 04:48:43 +0800

    add routes and endpoints for category feature

--- week06/backend/app/api/v1/endpoints/category.py
+++ week06/backend/app/api/v1/endpoints/category.py
@@ -1,10 +1,10 @@
-from fastapi import HTTPException, APIRouter
+from fastapi import APIRouter
 from starlette import status
 
 
 from backend.app.model import models
 from backend.app.model.category import Category, UpdateCategory
-from backend.app.utils import authentication_check, product_available
+from backend.app.utils.product_available import product_available
 from backend.app.core.config import user_dependency, db_dependency
 from backend.app.utils.authentication_check import authentication_check
 
@@ -12,7 +12,7 @@ from backend.app.utils.authentication_check import authentication_check
 
 
 router = APIRouter(
-    prefix= "category",
+    prefix= "/category",
     tags= ["category"]
 )
 
@@ -20,12 +20,9 @@ router = APIRouter(
 @router.post("/api/category", status_code=status.HTTP_201_CREATED, response_model=Category)
 async def create_category(info: Category, current_user: user_dependency, db: db_dependency):
     authentication_check(current_user)
-    # category= db.query(models.Categories).filter(models.Categories.name == info.name).first()
-    # if not category:
-    #     raise HTTPException(detail="category problem")
-    db_add= models.Products(
-        name= info.name,
-        id= info.id
+    db_add= models.Categories(
+        name= info.name
+        
     )
     db.add(db_add)
     db.commit()
@@ -35,12 +32,35 @@ async def create_category(info: Category, current_user: user_dependency, db: db_
 @router.get("/api/category", status_code=status.HTTP_200_OK)
 async def get_category(current_user: user_dependency, db: db_dependency):
     authentication_check(current_user)
-    results= db.query(models.Categories)
+    results= db.query(models.Categories).all()
     return results
 
 @router.get("/api/category/{id}", status_code=status.HTTP_200_OK)
-async def get_product_by_key(id: int, current_user: user_dependency, db: db_dependency):
+async def get_category_by_key(id: int, current_user: user_dependency, db: db_dependency):
     authentication_check(current_user)
     category_by_id= db.query(models.Categories).filter(models.Categories.id == id).first()
     product_available(category_by_id)
     return category_by_id
+
+@router.put("/api/category/{id}")
+async def update_category(id: int, updated_to: UpdateCategory, current_user: user_dependency, db: db_dependency):
+    authentication_check(current_user)
+    product_query= db.query(models.Categories).filter(models.Categories.id == id).first()
+    product_available(product_query)
+    if updated_to.name is not None:
+        product_query.name = updated_to.name
+
+    db.commit()
+    db.refresh(product_query)
+    return product_query
+
+
+@router.delete("/api/category/{id}") 
+async def delete_category(product_id: int, current_user: user_dependency, db: db_dependency):
+    authentication_check(current_user)
+    product_query= db.query(models.Categories).filter(models.Categories.id == product_id).first()
+    product_available(product_query)
+    db.delete(product_query)
+    db.refresh(product_query)
+    db.commit()
+ 
