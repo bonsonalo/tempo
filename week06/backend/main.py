99c22d60d1680947ca68c1bf223e5a95cce97ad5
bonsonalo <bonson2468@gmail.com>
2025-10-14 04:42:24 +0800

    chore: removed unwanted unprotected endpoint

--- week06/backend/main.py
+++ week06/backend/main.py
@@ -1,26 +1,19 @@
 from fastapi import FastAPI, HTTPException
 from backend.app.core import database
-from backend.app.model import models
 from backend.app.core.database import engine
 from starlette import status
 from backend.app.core.config import db_dependency, user_dependency
-from backend.app.api.v1.endpoints import auth, product
-
+from backend.app.api.v1.routes import routers
 
 
 
 app= FastAPI()
-app.include_router(auth.router)
-app.include_router(product.router)
+app.include_router(routers)
+
 
 database.base.metadata.create_all(bind= engine)
+ 
 
-
-@app.get("/user", status_code=status.HTTP_200_OK)
-def user(user: user_dependency, db: db_dependency):
-    if user is None:
-        raise HTTPException(status_code=401, detail="user not validated")
-    return {"User": user}
 
 
 
