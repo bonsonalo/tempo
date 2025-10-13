2a89ef62b247909517da06c2955bf440bef72c8c
bonsonalo <bonson2468@gmail.com>
2025-10-14 04:47:15 +0800

    add routes and endpoints for stock feature

---
+++ week06/backend/app/api/v1/endpoints/stock.py
@@ -0,0 +1,79 @@
+from fastapi import APIRouter, HTTPException
+from starlette import status 
+
+from backend.app.model import models
+from backend.app.model.stock import Stock, UpdateStock
+from backend.app.core.config import user_dependency, db_dependency
+from backend.app.utils.authentication_check import authentication_check
+from backend.app.utils.product_available import product_available
+
+
+router = APIRouter(
+    prefix= "/stock",
+    tags= ["stock"]
+)
+
+
+@router.post("/api/stock", status_code=status.HTTP_201_CREATED)
+async def create_stock(info: Stock, current_user: user_dependency, db: db_dependency):
+    authentication_check(current_user)
+    product_connect= db.query(models.Products).filter(models.Products.id == info.product_id).first()
+    if not product_connect:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="product name not found !")
+    supplier_connect= db.query(models.Suppliers).filter(models.Suppliers.id  == info.supplier_id).first()
+    if not supplier_connect:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="supplier name not found !")
+    db_add= models.Stock(
+        quantity= info.quantity,
+        movement_type= info.movement_type,
+        date= info.date,
+        supplier_id= supplier_connect.id,
+        product_id= product_connect.id
+
+    )
+    db.add(db_add)
+    db.commit()
+    db.refresh(db_add)
+    return db_add
+
+@router.get("/api/stock", status_code=status.HTTP_200_OK)
+async def get_stock(current_user: user_dependency, db: db_dependency):
+    authentication_check(current_user)
+    results= db.query(models.Stock).all()
+    return results
+
+@router.get("/api/stock/{id}", status_code=status.HTTP_200_OK)
+async def get_stock_by_key(id: int, current_user: user_dependency, db: db_dependency):
+    authentication_check(current_user)
+    category_by_id= db.query(models.Stock).filter(models.Stock.id == id).first()
+    product_available(category_by_id)
+    return category_by_id
+
+@router.put("/api/stock/{id}")
+async def update_stock(id: int, updated_to: UpdateStock, current_user: user_dependency, db: db_dependency):
+    authentication_check(current_user)
+    product_query= db.query(models.Stock).filter(models.Stock.id == id).first()
+    product_available(product_query)
+    if updated_to.product_id is not None:
+        product_query.product_id = updated_to.product_id
+    if updated_to.quantity is not None:
+        product_query.quantity= updated_to.quantity
+    if updated_to.movement_type is not None:
+        product_query.movement_type= updated_to.movement_type
+    if updated_to.date is not None:
+        product_query.date= updated_to.date
+    if updated_to.supplier_id is not None:
+        product_query.supplier_id= updated_to.supplier_id
+    db.commit()
+    db.refresh(product_query)
+    return product_query
+
+
+@router.delete("/api/category/{id}") 
+async def delete_stock(product_id: int, current_user: user_dependency, db: db_dependency):
+    authentication_check(current_user)
+    product_query= db.query(models.Stock).filter(models.Stock.id == product_id).first()
+    product_available(product_query)
+    db.delete(product_query)
+    db.refresh(product_query)
+    db.commit()
