8a3dbadb29980683c0d4fed7b03eda1acc5ed514
bonsonalo <bonson2468@gmail.com>
2025-10-20 00:38:48 +0800

    feat: add logger in the file

--- week06/backend/app/utils/product_available.py
+++ week06/backend/app/utils/product_available.py
@@ -1,8 +1,10 @@
 from fastapi import HTTPException
 from starlette import status
+from backend.app.core.logger import logger
 
 
 
 def product_available(value):
     if not value:
+        logger.error("product not found to delete")
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="product not found")
