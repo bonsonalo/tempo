36fbde7aa6541d44db5fa892e22ed53f1eaea149
bonsonalo <bonson2468@gmail.com>
2025-10-21 17:55:08 +0800

    chore: move all business logic to service directory

--- week06/backend/app/api/v1/endpoints/supplier.py
+++ week06/backend/app/api/v1/endpoints/supplier.py
@@ -2,6 +2,7 @@ from starlette import status
 from fastapi import APIRouter
 
 
+from backend.app.service.supplier_service import create_supplier_service, get_supplier_service, get_supplier_by_key_service, update_supplier_service, delete_supplier_service
 from backend.app.utils.authentication_check import authentication_check
 from backend.app.core.config import user_dependency, db_dependency, user_authentication_dependency, admin_dependency, superadmin_dependency
 from backend.app.model import models
@@ -15,53 +16,33 @@ from backend.app.utils.product_available import product_available
 
 
 router = APIRouter(
-    prefix= "/supplier",
+    prefix= "api/supplier",
     tags= ["supplier"]
 )
 
 
-@router.post("/api/suppliers", status_code=status.HTTP_201_CREATED)
+@router.post("/create", status_code=status.HTTP_201_CREATED)
 async def create_supplier(info: Supplier, current_user: admin_dependency, db: db_dependency):
     authentication_check(current_user)
-    db_add= models.Suppliers(
-        name= info.name
-    )
-    db.add(db_add)
-    db.commit()
-    db.refresh(db_add)
-    return db_add
+    return create_supplier_service(info, db)
 
-@router.get("/api/suppliers", status_code=status.HTTP_200_OK)
+@router.get("/get_all", status_code=status.HTTP_200_OK)
 async def get_supplier(current_user: user_dependency, db: db_dependency):
     authentication_check(current_user)
-    results= db.query(models.Suppliers).all()
-    return results
+    return get_supplier_service(db)
 
-@router.get("/api/suppliers/{id}", status_code=status.HTTP_200_OK)
+@router.get("/get/{id}", status_code=status.HTTP_200_OK)
 async def get_supplier_by_key(id: int, current_user: user_dependency, db: db_dependency):
     authentication_check(current_user)
-    category_by_id= db.query(models.Suppliers).filter(models.Suppliers.id == id).first()
-    product_available(category_by_id)
-    return category_by_id
+    return get_supplier_by_key_service(id, db)
 
-@router.put("/api/suppliers/{id}")
+@router.put("/update/{id}")
 async def update_supplier(id: int, updated_to: UpdateSupplier, current_user: admin_dependency, db: db_dependency):
     authentication_check(current_user)
-    product_query= db.query(models.Suppliers).filter(models.Suppliers.id == id).first()
-    product_available(product_query)
-    if updated_to.name is not None:
-        product_query.name = updated_to.name
-
-    db.commit()
-    db.refresh(product_query)
-    return product_query
+    return update_supplier_service(id, updated_to, db)
 
 
-@router.delete("/api/suppliers/{id}") 
+@router.delete("/delete/{product_id}") 
 async def delete_supplier(product_id: int, current_user: superadmin_dependency, db: db_dependency):
     authentication_check(current_user)
-    product_query= db.query(models.Suppliers).filter(models.Suppliers.id == product_id).first()
-    product_available(product_query)
-    db.delete(product_query)
-    db.refresh(product_query)
-    db.commit()
+    return delete_supplier_service(product_id, db)
