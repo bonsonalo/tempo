b9aa1aff4e27d372096a813b7eaaac4b6a9097d8
bonsonalo <bonson2468@gmail.com>
2025-10-21 15:04:40 +0800

    chore: moved business logic of create and get_all routes to category_service file

--- week06/backend/app/api/v1/endpoints/category.py
+++ week06/backend/app/api/v1/endpoints/category.py
@@ -1,13 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 from starlette import status
 
 
 from backend.app.model import models
 from backend.app.model.category import Category, UpdateCategory
+from backend.app.service.category_service import create_category_service, get_category_schema
 from backend.app.utils.product_available import product_available
 from backend.app.core.config import user_dependency, db_dependency, user_authentication_dependency, admin_dependency, superadmin_dependency
 from backend.app.utils.authentication_check import authentication_check
-
+from backend.app.core.logger import logger
 
 
 
@@ -20,20 +21,15 @@ router = APIRouter(
 @router.post("/create", status_code=status.HTTP_201_CREATED, response_model=Category)
 async def create_category(info: Category, current_user: admin_dependency, db: db_dependency):
     authentication_check(current_user)
-    db_add= models.Categories(
-        name= info.name
-        
-    )
-    db.add(db_add)
-    db.commit()
-    db.refresh(db_add)
-    return db_add
+    return create_category_service(info, db)
 
 @router.get("/get_all", status_code=status.HTTP_200_OK)
-async def get_category(current_user: user_dependency, db: db_dependency):
+async def get_category(current_user: user_dependency, db: db_dependency, 
+                       sort_by: str= Query("id"),
+                       order: str= Query("asc")):
     authentication_check(current_user)
-    results= db.query(models.Categories).all()
-    return results
+    return get_category_schema(db, sort_by, order)
+    
 
 @router.get("/get/{id}", status_code=status.HTTP_200_OK)
 async def get_category_by_key(id: int, current_user: user_dependency, db: db_dependency):
