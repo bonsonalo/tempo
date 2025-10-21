fef28d3f1d311efa84bd6ab962453c1d8bb510bc
bonsonalo <bonson2468@gmail.com>
2025-10-21 16:33:17 +0800

    chore: move the business logic of delete route to category_service file

--- week06/backend/app/api/v1/endpoints/category.py
+++ week06/backend/app/api/v1/endpoints/category.py
@@ -4,7 +4,7 @@ from starlette import status
 
 from backend.app.model import models
 from backend.app.model.category import Category, UpdateCategory
-from backend.app.service.category_service import create_category_service, get_category_schema, get_category_by_id_service
+from backend.app.service.category_service import create_category_service, get_category_schema, get_category_by_id_service, update_category_service, delete_category_service
 from backend.app.utils.product_available import product_available
 from backend.app.core.config import user_dependency, db_dependency, user_authentication_dependency, admin_dependency, superadmin_dependency
 from backend.app.utils.authentication_check import authentication_check
@@ -43,21 +43,12 @@ async def get_category_by_id(id: int, current_user: user_dependency, db: db_depe
 @router.put("/update/{id}")
 async def update_category(id: int, updated_to: UpdateCategory, current_user: admin_dependency, db: db_dependency):
     authentication_check(current_user)
-    product_query= db.query(models.Categories).filter(models.Categories.id == id).first()
-    if updated_to.name is not None:
-        product_query.name = updated_to.name
-
-    db.commit()
-    db.refresh(product_query)
-    return product_query
+    return update_category_service(id, updated_to, db)
 
 
-@router.delete("/delete/{id}") 
+@router.delete("/delete/{product_id}") 
 async def delete_category(product_id: int, current_user: superadmin_dependency, db: db_dependency):
     authentication_check(current_user)
-    product_query= db.query(models.Categories).filter(models.Categories.id == product_id).first()
-    product_available(product_query)
-    db.delete(product_query)
-    db.refresh(product_query)
-    db.commit()
+    logger.info("successfully authorized")
+    return delete_category_service(product_id, db)
  
